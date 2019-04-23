7fc2e21558a12b1b4fed58aff3a876d01ccda995
bosh <810916259@qq.com>
2019-04-23 11:22:11 +0800

    android P 兼容问题

--- Demo/app/src/main/java/com/china/bosh/demo/application/MyApplication.java
+++ Demo/app/src/main/java/com/china/bosh/demo/application/MyApplication.java
@@ -6,6 +6,10 @@ import android.support.multidex.MultiDex;
 import com.china.bosh.mylibrary.application.BaseApplication;
 import com.china.bosh.mylibrary.db.DaoManager;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 /**
  * @author lzq
  * @date 2018/9/6
@@ -17,11 +21,37 @@ public class MyApplication extends BaseApplication {
     public void onCreate() {
         super.onCreate();
         DaoManager.getInstance().init(this);
+
+        closeAndroidPDialog();
     }
 
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         MultiDex.install(this);
+    }
+
+    /**
+     * 解决android P出现Detected problems with API compatibility 弹窗问题
+     */
+    private void closeAndroidPDialog(){
+        try {
+            Class aClass = Class.forName("android.content.pm.PackageParser$Package");
+            Constructor declaredConstructor = aClass.getDeclaredConstructor(String.class);
+            declaredConstructor.setAccessible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            Class cls = Class.forName("android.app.ActivityThread");
+            Method declaredMethod = cls.getDeclaredMethod("currentActivityThread");
+            declaredMethod.setAccessible(true);
+            Object activityThread = declaredMethod.invoke(null);
+            Field mHiddenApiWarningShown = cls.getDeclaredField("mHiddenApiWarningShown");
+            mHiddenApiWarningShown.setAccessible(true);
+            mHiddenApiWarningShown.setBoolean(activityThread, true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
