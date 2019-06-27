7540028955954ef416e7d3b3d59b53bc2470540b
bosh <810916259@qq.com>
2019-06-27 10:52:38 +0800

    gradle升级到5.1.1

--- module_demo/app/src/main/java/com/bosh/module_demo_test/MainActivity.java
+++ module_demo/app/src/main/java/com/bosh/module_demo_test/MainActivity.java
@@ -2,6 +2,8 @@ package com.bosh.module_demo_test;
 
 import android.os.Bundle;
 
+import com.alibaba.android.arouter.launcher.ARouter;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
@@ -12,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        ARouter.getInstance().build("/demo/demoMain").navigation();
+        finish();
     }
 }
--- module_demo/app/src/main/java/com/bosh/module_demo_test/MyApplication.java
+++ module_demo/app/src/main/java/com/bosh/module_demo_test/MyApplication.java
@@ -2,6 +2,7 @@ package com.bosh.module_demo_test;
 
 import android.content.Context;
 
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.china.bosh.mylibrary.application.BaseApplication;
 
 import androidx.multidex.MultiDex;
@@ -18,5 +19,16 @@ public class MyApplication extends BaseApplication {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         MultiDex.install(this);
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if(BuildConfig.DEBUG) {
+            ARouter.openLog();
+            ARouter.openDebug();
+        }
+        ARouter.init(this);
     }
 }
